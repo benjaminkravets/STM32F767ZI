b1388c27787f01882edd757f1a707ea5fe30e6b3
Benjamin Kravets <bakravets@gmail.com>
2023-05-31 20:18:55 -0400

    add freertos

---
+++ uart_isr/Debug/Core/Inc/SEGGER_RTT_ASM_ARMv7M.d
@@ -0,0 +1,4 @@
+Core/Inc/SEGGER_RTT_ASM_ARMv7M.o: ../Core/Inc/SEGGER_RTT_ASM_ARMv7M.S \
+ ../Core/Inc/SEGGER_RTT.h ../Core/Inc/SEGGER_RTT_Conf.h
+../Core/Inc/SEGGER_RTT.h:
+../Core/Inc/SEGGER_RTT_Conf.h:
---
+++ uart_isr/Debug/Core/Inc/SEGGER_RTT_Syscalls_KEIL.d
@@ -0,0 +1,2 @@
+Core/Inc/SEGGER_RTT_Syscalls_KEIL.o: \
+ ../Core/Inc/SEGGER_RTT_Syscalls_KEIL.c
