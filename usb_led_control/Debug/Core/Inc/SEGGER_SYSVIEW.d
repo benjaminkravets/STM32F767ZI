e3db69676063d3ec2ccbdc8dc4db88284d1b4792
Benjamin Kravets <bakravets@gmail.com>
2023-07-10 20:43:52 -0400

    backup

---
+++ usb_led_control/Debug/Core/Inc/SEGGER_SYSVIEW.d
@@ -0,0 +1,14 @@
+Core/Inc/SEGGER_SYSVIEW.o: ../Core/Inc/SEGGER_SYSVIEW.c \
+ ../Core/Inc/SEGGER_SYSVIEW_Int.h ../Core/Inc/SEGGER_SYSVIEW.h \
+ ../Core/Inc/SEGGER.h ../Core/Inc/Global.h \
+ ../Core/Inc/SEGGER_SYSVIEW_ConfDefaults.h \
+ ../Core/Inc/SEGGER_SYSVIEW_Conf.h ../Core/Inc/SEGGER_RTT_Conf.h \
+ ../Core/Inc/SEGGER_RTT.h
+../Core/Inc/SEGGER_SYSVIEW_Int.h:
+../Core/Inc/SEGGER_SYSVIEW.h:
+../Core/Inc/SEGGER.h:
+../Core/Inc/Global.h:
+../Core/Inc/SEGGER_SYSVIEW_ConfDefaults.h:
+../Core/Inc/SEGGER_SYSVIEW_Conf.h:
+../Core/Inc/SEGGER_RTT_Conf.h:
+../Core/Inc/SEGGER_RTT.h:
