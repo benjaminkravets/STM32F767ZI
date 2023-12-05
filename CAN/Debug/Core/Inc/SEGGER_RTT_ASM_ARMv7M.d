c98c7eb1a63ca19345eab5db12ffec47fdb0c517
Benjamin Kravets <bakravets@gmail.com>
2023-12-04 19:10:34 -0500

    rm old can test projects

--- CAN/Debug/Core/Inc/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,4 +0,0 @@
-Core/Inc/SEGGER_RTT_ASM_ARMv7M.o: ../Core/Inc/SEGGER_RTT_ASM_ARMv7M.S \
- ../Core/Inc/SEGGER_RTT.h ../Core/Inc/SEGGER_RTT_Conf.h
-../Core/Inc/SEGGER_RTT.h:
-../Core/Inc/SEGGER_RTT_Conf.h:
