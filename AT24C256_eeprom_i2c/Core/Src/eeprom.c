b99752e774b0b6ddd10276eba3ad49855ac7c512
Benjamin Kravets <bakravets@gmail.com>
2024-03-15 20:52:19 -0400

    add write bytes function

--- AT24C256_eeprom_i2c/Core/Src/eeprom.c
+++ AT24C256_eeprom_i2c/Core/Src/eeprom.c
@@ -14,6 +14,7 @@ extern I2C_HandleTypeDef hi2c1;
 
 //AT24C256 address (all pins 0)
 #define EEPROM_ADDR 0xA0
+#define EEPROM_I2C &hi2c1
 
 
 //512 64 byte pages
@@ -27,7 +28,7 @@ uint16_t remaining_bytes (uint16_t size, uint16_t offset){
 }
 
 /*
- * void eeprom_write_bytes(uint16_t page, uint16_t offset, uint8_t* data, uint16_t size);
+ * void eeprom_write_bytes(uint16_t page, uint16_t offset, uint8_t* data, uint16_t size)
  * Write bytes to AT24C256.
  * @param page to address within page count
  * @param offset bytes into the page within page size
@@ -38,5 +39,65 @@ uint16_t remaining_bytes (uint16_t size, uint16_t offset){
 
 void eeprom_write_bytes(uint16_t page, uint16_t offset, uint8_t* data, uint16_t size){
 	int paddrposition = log(PAGE_SIZE) / log(2);
+
+	uint16_t startPage = page;
+	uint16_t endPage = page + ((size+offset)/PAGE_SIZE);
+
+	uint16_t numofpages = (endPage-startPage) + 1;
+	uint16_t pos=0;
+
+	for (int i = 0; i<numofpages; i++){
+		uint16_t MemAddress = startPage<<paddrposition | offset;
+		uint16_t bytesremaining = remaining_bytes(size, offset);
+
+		HAL_I2C_Mem_Write(EEPROM_I2C, EEPROM_ADDR, MemAddress, 2, &data[pos], bytesremaining, 1000);
+
+		startPage += 1;
+		offset = 0;
+		size = size-bytesremaining;
+		pos += bytesremaining;
+
+		HAL_Delay(5);
+	}
 }
-void eeprom_read_bytes(uint16_t page, uint16_t offset, uint8_t* data, uint16_t size);
+
+/*
+ * void eeprom_read_bytes(uint16_t page, uint16_t offset, uint8_t* data, uint16_t size)
+ * Write bytes to AT24C256.
+ * @param page to address within page count
+ * @param offset bytes into the page within page size
+ * @param data pointer to which data is read
+ * @param size how many bytes to read
+ *
+ */
+
+void eeprom_read_bytes(uint16_t page, uint16_t offset, uint8_t* data, uint16_t size){
+	int paddrposition = log(PAGE_SIZE) / log(2);
+
+	uint16_t startPage = page;
+	uint16_t endPage = page + ((size+offset)/PAGE_SIZE);
+
+	uint16_t numofpages = (endPage-startPage) + 1;
+	uint16_t pos=0;
+
+	for (int i = 0; i<numofpages; i++){
+		uint16_t MemAddress = startPage<<paddrposition | offset;
+		uint16_t bytesremaining = remaining_bytes(size, offset);
+		HAL_I2C_Mem_Read(EEPROM_I2C, EEPROM_ADDR, MemAddress, 2, &data[pos], bytesremaining, 1000);
+
+		startPage += 1;
+		offset = 0;
+		size = size-bytesremaining;
+		pos += bytesremaining;
+
+	}
+}
+/*
+ * void eeprom_erasepage(uint16_t page)
+ * Erase page
+ * @param page to erase
+ */
+
+void eeprom_erasepage(uint16_t page){
+	int
+}
