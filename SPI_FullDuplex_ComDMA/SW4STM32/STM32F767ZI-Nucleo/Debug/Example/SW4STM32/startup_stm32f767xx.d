b4699dd5b7a1d82c11157c1da9346a311b86a369
Benjamin Kravets <bakravets@gmail.com>
2024-04-19 21:22:00 -0400

    rm stm32 example projects

--- SPI_FullDuplex_ComDMA/SW4STM32/STM32F767ZI-Nucleo/Debug/Example/SW4STM32/startup_stm32f767xx.d
+++
@@ -1,2 +0,0 @@
-Example/SW4STM32/startup_stm32f767xx.o: \
- C:/Users/bakra/Desktop/STM32F767ZI/SPI_FullDuplex_ComDMA/SW4STM32/startup_stm32f767xx.s
--- SPI_TwoBoards_FullDuplex_DMA/SW4STM32/STM32F767ZI-Nucleo/Debug/Example/SW4STM32/startup_stm32f767xx.d
+++
@@ -1,2 +0,0 @@
-Example/SW4STM32/startup_stm32f767xx.o: \
- C:/Users/bakra/Desktop/STM32F767ZI/SPI_TwoBoards_FullDuplex_DMA/SW4STM32/startup_stm32f767xx.s
