a73625b9567eb2f2b13eb2c1de361f9447dc0b26
Benjamin Kravets <bakravets@gmail.com>
2025-09-13 16:01:37 -0400

    rest controlled stepper

---
+++ stepper_control/Core/Inc/mongoose_config.h
@@ -0,0 +1,14 @@
+/*
+ * mongoose_config.h
+ *
+ *  Created on: Sep 13, 2025
+ *      Author: bakra
+ */
+
+#ifndef INC_MONGOOSE_CONFIG_H_
+#define INC_MONGOOSE_CONFIG_H_
+
+#pragma once
+#define MG_ARCH MG_ARCH_CUBE
+
+#endif /* INC_MONGOOSE_CONFIG_H_ */
