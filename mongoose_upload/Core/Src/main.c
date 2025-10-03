08fce154ee254ae6892d44dcac3d85a49bc961be
Benjamin Kravets <bakravets@gmail.com>
2025-10-03 19:44:34 -0400

    remove comments, format

--- mongoose_upload/Core/Src/main.c
+++ mongoose_upload/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -24,6 +24,7 @@
 /* USER CODE BEGIN Includes */
 #include "mongoose.h"
 #include "sodium.h"
+#include "public_key.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,7 +34,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define MAX_IMAGE_SIZE 99999
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,21 +43,21 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-#if defined ( __ICCARM__ ) /*!< IAR Compiler */
-#pragma location=0x2007c000
-ETH_DMADescTypeDef  DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
-#pragma location=0x2007c0a0
-ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
+#if defined(__ICCARM__) /*!< IAR Compiler */
+#pragma location = 0x2007c000
+ETH_DMADescTypeDef DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
+#pragma location = 0x2007c0a0
+ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
 
-#elif defined ( __CC_ARM )  /* MDK ARM Compiler */
+#elif defined(__CC_ARM) /* MDK ARM Compiler */
 
-__attribute__((at(0x2007c000))) ETH_DMADescTypeDef  DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
-__attribute__((at(0x2007c0a0))) ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
+__attribute__((at(0x2007c000))) ETH_DMADescTypeDef DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
+__attribute__((at(0x2007c0a0))) ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
 
-#elif defined ( __GNUC__ ) /* GNU Compiler */
+#elif defined(__GNUC__) /* GNU Compiler */
 
 ETH_DMADescTypeDef DMARxDscrTab[ETH_RX_DESC_CNT] __attribute__((section(".RxDecripSection"))); /* Ethernet Rx DMA Descriptors */
-ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT] __attribute__((section(".TxDecripSection")));   /* Ethernet Tx DMA Descriptors */
+ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT] __attribute__((section(".TxDecripSection"))); /* Ethernet Tx DMA Descriptors */
 #endif
 
 ETH_TxPacketConfig TxConfig;
@@ -76,72 +77,76 @@ static void MX_GPIO_Init(void);
 static void MX_ETH_Init(void);
 static void MX_UART4_Init(void);
 /* USER CODE BEGIN PFP */
-uint32_t blank_random() {
-	return 0;
-}
 
-void blank_buf(void * const buf, const size_t size) {
-	memset(buf, 0, size);
-}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-static void mylog(char ch, void *param) {
-	HAL_UART_Transmit(&huart4, &ch, 1, 100);
 
-}
-
-//static void fn(struct mg_connection *c, int ev, void *ev_data) {
-//	if (ev == MG_EV_HTTP_MSG) mg_http_reply(c, 200, "", "ok\n");
-//}
-
-//static void fn(struct mg_connection *c, int ev, void *ev_data) {
-//	if (ev == MG_EV_HTTP_MSG) {
-//		struct mg_http_serve_opts opts = {
-//				.root_dir = "/web_root",
-//				.fs = &mg_fs_packed
-//		};
-//		mg_http_serve_dir(c, ev_data, &opts);
-//	}
-//}
-
-struct firmware_image {
-  char * data;
-  char data_sha256[32];
-  char data_signature[64];
-  size_t size;
-} img0;
-
-int init_firmware_image(struct firmware_image * image) {
-  image->data = malloc(200000);
-  assert(img0.data != NULL);
-  image->size = 0;
+uint32_t blank_random()
+{
   return 0;
 }
 
-int verify_firmware_image(struct firmware_image * image) {
-	memcpy(&image->data_sha256, image->data, 32);
-	memcpy(&image->data_signature, image->data + 32, 64);
+void blank_buf(void *const buf, const size_t size)
+{
+  memset(buf, 0, size);
+}
 
-	char calculated_data_sha256[32];
+static void mylog(char ch, void *param)
+{
+  HAL_UART_Transmit(&huart4, &ch, 1, 100);
+}
 
-	//crypto_hash_sha256(calculated_data_sha256, &image->, inlen)
+struct firmware_image
+{
+  char *data;
+  char *data_sha256;
+  char *data_signature;
+  char *file_data;
+  size_t size;
+} main_img;
 
-	__BKPT(0);
+int init_firmware_image(struct firmware_image *image)
+{
+  assert((image->data = malloc(MAX_IMAGE_SIZE)) != NULL);
+  image->size = 0;
+  image->data_sha256 = image->data + 0;
+  image->data_signature = image->data + 32;
+  image->file_data = image->data + 96;
+  return 0;
+}
+
+int verify_firmware_image(struct firmware_image *image)
+{
+
+  uint8_t calculated_data_sha256[32] = {0};
+
+  crypto_hash_sha256(calculated_data_sha256, image->file_data, image->size - 96);
+  if (memcmp(calculated_data_sha256, image->data_sha256, 32) != 0)
+  {
+    printf("SHA2 doesn't match \r\n");
+    assert(1);
+  }
+  if (crypto_sign_ed25519_verify_detached(image->data_signature, calculated_data_sha256, 32, public_key) != 0)
+  {
+    printf("Signature doesn't match \r\n");
+    assert(1);
+  }
+  printf("checks out \r\n");
 
 }
 
-int print_firmware_image(struct firmware_image * image) {
-  for(int i = 0 ; i < image->size ; i++) {
+int print_firmware_image(struct firmware_image *image)
+{
+  for (int i = 0; i < image->size; i++)
+  {
     printf("%c", image->data[i]);
   }
   verify_firmware_image(image);
 }
 
-
-
-long mg_http_upload_modified(struct mg_connection *c, struct mg_http_message *hm, size_t max_size, struct firmware_image * img)
+long mg_http_upload_modified(struct mg_connection *c, struct mg_http_message *hm, size_t max_size, struct firmware_image *img)
 {
   char buf[20] = "0", file[MG_PATH_MAX], path[MG_PATH_MAX], total[MG_PATH_MAX];
   long res = 0, offset, total_length;
@@ -150,44 +155,61 @@ long mg_http_upload_modified(struct mg_connection *c, struct mg_http_message *hm
   mg_http_get_var(&hm->query, "total", total, sizeof(total));
   offset = strtol(buf, NULL, 0);
   total_length = strtol(total, NULL, 0);
-  if (hm->body.len == 0) {
-    mg_http_reply(c, 200, "", "%ld", res);  // Nothing to write
-  } else if (file[0] == '\0') {
+  if (hm->body.len == 0)
+  {
+    mg_http_reply(c, 200, "", "%ld", res); // Nothing to write
+  }
+  else if (file[0] == '\0')
+  {
     mg_http_reply(c, 400, "", "file required");
     res = -1;
-  } else if (mg_path_is_sane(mg_str(file)) == false) {
+  }
+  else if (mg_path_is_sane(mg_str(file)) == false)
+  {
     mg_http_reply(c, 400, "", "%s: invalid file", file);
     res = -2;
-  } else if (offset < 0) {
+  }
+  else if (offset < 0)
+  {
     mg_http_reply(c, 400, "", "offset required");
     res = -3;
-  } else if ((size_t) offset + hm->body.len > max_size) {
+  }
+  else if ((size_t)offset + hm->body.len > max_size)
+  {
     mg_http_reply(c, 400, "", "%s: over max size of %lu", path,
-                  (unsigned long) max_size);
+                  (unsigned long)max_size);
     res = -4;
-  } else {
+  }
+  else
+  {
     size_t current_size = 0;
     MG_DEBUG(("%s -> %lu bytes @ %ld", path, hm->body.len, offset));
-    if (offset == 0) img->size = 0; //if offset is 0, "delete" the firmware image by starting write from 0
+    if (offset == 0)
+      img->size = 0; // if offset is 0, "delete" the firmware image by starting write from 0
     current_size = img->size;
-    if (offset > 0 && current_size != (size_t) offset) {
+    if (offset > 0 && current_size != (size_t)offset)
+    {
       mg_http_reply(c, 400, "", "%s: offset mismatch", path);
       res = -5;
-    } else {
-      //printf("len is %i", hm->body.len);
+    }
+    else
+    {
+      // printf("len is %i", hm->body.len);
+      // coppy received http buffer to image buffer
       memcpy(img->data + img->size, hm->body.buf, hm->body.len);
-       //printf("%s \r\n", hm->body.buf);
-        for(int i = 0 ; i < hm->body.len ; i++) {
-          printf("%c", hm->body.buf[i]);
-        }
+      // printf("%s \r\n", hm->body.buf);
+      for (int i = 0; i < hm->body.len; i++)
+      {
+        printf("%c", hm->body.buf[i]);
+      }
 
       printf("offset %i \r\n", offset);
       img->size += hm->body.len;
       res = hm->body.len;
-      if (total_length == img->size) {
+      if (total_length == img->size)
+      {
         printf("transfer complete, total length %i current image length %i \r\n", total_length, img->size);
         print_firmware_image(img);
-//        verify_firmware_image(img);
       }
 
       mg_http_reply(c, 200, "", "%ld", res);
@@ -197,26 +219,29 @@ long mg_http_upload_modified(struct mg_connection *c, struct mg_http_message *hm
   return res;
 }
 
-static void fn(struct mg_connection *c, int ev, void *ev_data) {
-	if (ev == MG_EV_HTTP_MSG) {
-		struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-		if (mg_match(hm->uri, mg_str("/upload"), NULL)) {
-			//mg_http_upload(c, hm, &mg_fs_posix, "/tmp", 99999);
-			//HAL_UART_Transmit(&huart4, hm->body.buf, hm->body.len, 100);
-			mg_http_upload_modified(c, hm, 99999, &img0);
-
-		} else {
-			struct mg_http_serve_opts opts = {.root_dir = "/web_root", .fs = &mg_fs_packed};
-			mg_http_serve_dir(c, ev_data, &opts);
-		}
-	}
+static void fn(struct mg_connection *c, int ev, void *ev_data)
+{
+  if (ev == MG_EV_HTTP_MSG)
+  {
+    struct mg_http_message *hm = (struct mg_http_message *)ev_data;
+    if (mg_match(hm->uri, mg_str("/upload"), NULL))
+    {
+      // mg_http_upload(c, hm, &mg_fs_posix, "/tmp", 99999);
+      mg_http_upload_modified(c, hm, MAX_IMAGE_SIZE, &main_img);
+    }
+    else
+    {
+      struct mg_http_serve_opts opts = {.root_dir = "/web_root", .fs = &mg_fs_packed};
+      mg_http_serve_dir(c, ev_data, &opts);
+    }
+  }
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -245,56 +270,33 @@ int main(void)
   MX_ETH_Init();
   MX_UART4_Init();
   /* USER CODE BEGIN 2 */
+  //initialize sodium
   randombytes_implementation blank = {
-		  .implementation_name = "blank",
-		  .random = blank_random,
-		  .buf = blank_buf,
+      .implementation_name = "blank",
+      .random = blank_random,
+      .buf = blank_buf,
   };
 
   randombytes_set_implementation(&blank);
 
-
-  if (sodium_init() == -1) {
-	  return 1;
+  if (sodium_init() == -1)
+  {
+    return 1;
   }
-  mg_log_set(MG_LL_DEBUG );
+  //initialize mongoose
+  mg_log_set(MG_LL_DEBUG);
   mg_log_set_fn(mylog, NULL);
 
   struct mg_mgr mgr;
   mg_mgr_init(&mgr);
   mg_http_listen(&mgr, "http://0.0.0.0", fn, &mgr);
 
-  init_firmware_image(&img0);
+  init_firmware_image(&main_img);
 
-  for (;;) mg_mgr_poll(&mgr, 50);
+  for (;;)
+    mg_mgr_poll(&mgr, 50);
 
-
-  uint8_t hash_me[10] = "0123456789";
-  uint8_t hash[32] = {0};
-
-  crypto_hash_sha256(hash, hash_me, 10);
-
-  uint8_t pk[] = {
-		    0x0e, 0x45, 0x8b, 0x35, 0xfb, 0x06, 0xe6, 0x9e,
-		    0xfa, 0x97, 0x07, 0x61, 0x13, 0x16, 0x0c, 0xc3,
-		    0x33, 0x5e, 0x4c, 0x06, 0xc0, 0x09, 0x3b, 0xb9,
-		    0x12, 0x78, 0xdf, 0x0e, 0x5b, 0x81, 0x9b, 0x84
-  };
-
-  uint8_t sig[] = {
-		  0xe4, 0x9a, 0x9f, 0x5f, 0x11, 0x32, 0x0d, 0xae,
-		  0x8a, 0xb1, 0x80, 0x1f, 0xde, 0xa0, 0x1f, 0xe3,
-		  0x6f, 0x0d, 0x63, 0x75, 0xcd, 0x3a, 0x6c, 0xe9,
-		  0xd7, 0xad, 0xcf, 0x52, 0x70, 0xdb, 0xf7, 0x8c,
-		  0x98, 0x05, 0xb0, 0x86, 0x3d, 0x50, 0xd0, 0x5e,
-		  0xba, 0xb4, 0x0e, 0x63, 0xf9, 0x25, 0x27, 0x94,
-		  0xdd, 0xf1, 0x90, 0xfc, 0x81, 0x3b, 0xab, 0xa6,
-		  0xf0, 0xdf, 0xf2, 0x29, 0x71, 0x21, 0x2a, 0x02
-		};
-
-
-
-  uint32_t ret = crypto_sign_ed25519_verify_detached(sig, hash, 32, pk);
+  free(main_img.data);
 
 
   /* USER CODE END 2 */
@@ -303,8 +305,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_7);
-	HAL_Delay(100);
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_7);
+    HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -313,22 +315,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -345,16 +347,15 @@ void SystemClock_Config(void)
   }
 
   /** Activate the Over-Drive mode
-  */
+   */
   if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -367,10 +368,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief ETH Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief ETH Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_ETH_Init(void)
 {
 
@@ -378,7 +379,7 @@ static void MX_ETH_Init(void)
 
   /* USER CODE END ETH_Init 0 */
 
-   static uint8_t MACAddr[6];
+  static uint8_t MACAddr[6];
 
   /* USER CODE BEGIN ETH_Init 1 */
 
@@ -405,21 +406,20 @@ static void MX_ETH_Init(void)
     Error_Handler();
   }
 
-  memset(&TxConfig, 0 , sizeof(ETH_TxPacketConfig));
+  memset(&TxConfig, 0, sizeof(ETH_TxPacketConfig));
   TxConfig.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
   TxConfig.ChecksumCtrl = ETH_CHECKSUM_IPHDR_PAYLOAD_INSERT_PHDR_CALC;
   TxConfig.CRCPadCtrl = ETH_CRC_PAD_INSERT;
   /* USER CODE BEGIN ETH_Init 2 */
 
   /* USER CODE END ETH_Init 2 */
-
 }
 
 /**
-  * @brief UART4 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief UART4 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_UART4_Init(void)
 {
 
@@ -447,14 +447,13 @@ static void MX_UART4_Init(void)
   /* USER CODE BEGIN UART4_Init 2 */
 
   /* USER CODE END UART4_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -471,7 +470,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOG_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LD1_Pin | LD3_Pin | LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
@@ -483,14 +482,14 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
-  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
+  GPIO_InitStruct.Pin = LD1_Pin | LD3_Pin | LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : STLK_RX_Pin STLK_TX_Pin */
-  GPIO_InitStruct.Pin = STLK_RX_Pin|STLK_TX_Pin;
+  GPIO_InitStruct.Pin = STLK_RX_Pin | STLK_TX_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -511,7 +510,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : USB_SOF_Pin USB_ID_Pin USB_DM_Pin USB_DP_Pin */
-  GPIO_InitStruct.Pin = USB_SOF_Pin|USB_ID_Pin|USB_DM_Pin|USB_DP_Pin;
+  GPIO_InitStruct.Pin = USB_SOF_Pin | USB_ID_Pin | USB_DM_Pin | USB_DP_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -534,9 +533,9 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -549,12 +548,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
